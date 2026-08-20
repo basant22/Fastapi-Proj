e49e68895b41184ec87649a116d6793e562d8711
basant22 <basant.kumar22@gmail.com>
2026-08-20 14:26:06 +0530

    change in test_main

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 def test_get_todos_for_empty(client):
-    response = client.get("todos")
+    response = client.get("/todos")
     assert response.status_code == 200
     data = response.json()
     assert response['message'] == "fetched all todos successfully"
